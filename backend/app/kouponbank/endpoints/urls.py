34263ee6de37506cbabd9114e58e7ff1dc59f037
Sung Ahn <sahn1998@gmail.com>
2020-08-24 20:22:27 -0700

    made the views into generic class-based views

--- backend/app/kouponbank/endpoints/urls.py
+++ backend/app/kouponbank/endpoints/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
 from . import views
 
+urlpatterns = [
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.User.as_view(), name='user')
+]
 
+app_name = 'users'
--- backend/app/kouponbank/endpoints/views.py
+++ backend/app/kouponbank/endpoints/views.py
@@ -1,12 +1,18 @@
 from kouponbank.database.models import User
 from kouponbank.database.serializers import UserSerializer
 
-from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 # Generic Viewsets로 하는게 더 좋을수도 있을것 같은데
 # 한번 다른 블로거들이 어떻게 했는지 봐야할듯.
 
-class UserViewset(viewsets.ModelViewSet):
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class User(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
