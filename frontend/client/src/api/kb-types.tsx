2bc221c2102e36461ab8693b3b05255febb776fc
Sung Ahn <sahn1998@gmail.com>
2020-09-17 15:20:54 -0700

    added comments

--- frontend/client/src/api/kb-types.tsx
+++ frontend/client/src/api/kb-types.tsx
@@ -1,7 +1,6 @@
 /**
  * Represents the required properties of a User
  */
-
 export interface User {
     id?: number;
     username: string;
--- frontend/client/src/store/index.tsx
+++ frontend/client/src/store/index.tsx
@@ -3,4 +3,5 @@ import thunk from "redux-thunk";
 import { reducer } from "./reducer";
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
